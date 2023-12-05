6d914b6443a87595e9105f56004cc9e3e43cfeaf
Boris Lechner <148430302+Boris-INSA@users.noreply.github.com>
2023-12-05 11:24:07 +0100

    ✨ Added a PostgreSQL datasource plugin

---
+++ plugins/datasources/postgresql/README.md
@@ -0,0 +1,79 @@
+<!--
+Hermes : Change Data Capture (CDC) tool from any source(s) to any target
+Copyright (C) 2023 INSA Strasbourg
+
+This file is part of Hermes.
+
+Hermes is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hermes is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hermes. If not, see <https://www.gnu.org/licenses/>.
+-->
+
+# `postgresql` datasource plugin
+
+## Description
+
+This plugin allows using an PostgreSQL database as datasource.
+
+## Configuration
+
+Connection settings are required in plugin configuration.
+
+```yaml
+hermes:
+  plugins:
+    datasources:
+      # Source name. Use whatever you want. Will be used in datamodel
+      your_source_name:
+        type: postgresql
+        settings:
+          # MANDATORY: the database server DNS name or IP address
+          server: dummy.example.com
+          # MANDATORY: the database connection port
+          port: 1234
+          # MANDATORY: the database name
+          dbname: DUMMY
+          # MANDATORY: the database credentials to use
+          login: HERMES_DUMMY
+          password: "DuMmY_p4s5w0rD"
+```
+
+## Usage
+
+Specify a query. If you'd like to provide values from cache, you should provide them in a `vars` dict, and refer to them by specifying the var key name encased ins `%()s` in the query: this will automatically sanitize the query. See example below.
+
+The example `vars` names are prefixed with `sanitized_` only for clarity, it's not a requirement.
+
+```yaml
+hermes-server:
+  datamodel:
+    oneDataType:
+      sources:
+        your_source_name: # 'your_source_name' was set in plugin settings
+          fetch:
+            type: fetch
+            query: >-
+              SELECT {{ REMOTE_ATTRIBUTES | join(', ') }}
+              FROM A_POSTGRESQL_TABLE
+
+          commit_one:
+            type: modify
+            query: >-
+              UPDATE A_POSTGRESQL_TABLE
+              SET
+                valueToSet = %(sanitized_valueToSet)s
+              WHERE pkey = %(sanitized_pkey)s
+
+            vars:
+              sanitized_pkey: "{{ ITEM_FETCHED_VALUES.pkey }}"
+              sanitized_valueToSet: "{{ ITEM_FETCHED_VALUES.valueToSet }}"
+```
---
+++ plugins/datasources/postgresql/postgresql.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Hermes : Change Data Capture (CDC) tool from any source(s) to any target
+# Copyright (C) 2023 INSA Strasbourg
+#
+# This file is part of Hermes.
+#
+# Hermes is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hermes is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hermes. If not, see <https://www.gnu.org/licenses/>.
+
+
+from typing import Any
+
+from lib.plugins import AbstractDataSourcePlugin
+import psycopg
+from psycopg.rows import dict_row
+
+import logging
+
+logger = logging.getLogger("hermes")
+
+HERMES_PLUGIN_CLASSNAME: str | None = "DatasourcePostgresql"
+"""The plugin class name defined in this module file"""
+
+
+class DatasourcePostgresql(AbstractDataSourcePlugin):
+    """Remote Data Source for Postgresql database"""
+
+    def __init__(self, settings: dict[str, Any]):
+        """Instantiate new plugin and store a copy of its settings dict in self._settings"""
+        super().__init__(settings)
+        self._dbcon: psycopg.Connection | None = None
+
+    def open(self):
+        """Establish connection with Postgresql Database"""
+        self._dbcon = psycopg.connect(
+            dbname=self._settings["dbname"],
+            user=self._settings["login"],
+            password=self._settings["password"],
+            host=self._settings["server"],
+            port=self._settings["port"],
+        )
+
+    def close(self):
+        """Close connection with Postgresql Database"""
+        self._dbcon.close()
+        self._dbcon = None
+
+    def fetch(
+        self,
+        query: str | None,
+        vars: dict[str, Any],
+    ) -> list[dict[str, Any]]:
+        """Fetch data from datasource with specified query and optional queryvars.
+        Returns a list of dict containing each entry fetched, with REMOTE_ATTRIBUTES
+        as keys, and corresponding fetched values as values"""
+        fetcheddata = []
+        with self._dbcon.cursor(row_factory=dict_row) as cur:
+            cur.execute(query, vars)
+            for row in cur:
+                fetcheddata.append(row)
+        return fetcheddata
+
+    def add(self, query: str | None, vars: dict[str, Any]):
+        """Add data to datasource with specified query and optional queryvars"""
+        with self._dbcon.cursor() as cur:
+            cur.execute(query, vars)
+        self._dbcon.commit()
+
+    def delete(self, query: str | None, vars: dict[str, Any]):
+        """Delete data from datasource with specified query and optional queryvars"""
+        with self._dbcon.cursor() as cur:
+            cur.execute(query, vars)
+        self._dbcon.commit()
+
+    def modify(self, query: str | None, vars: dict[str, Any]):
+        """Modify data on datasource with specified query and optional queryvars"""
+        with self._dbcon.cursor() as cur:
+            cur.execute(query, vars)
+        self._dbcon.commit()
