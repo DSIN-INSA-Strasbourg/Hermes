DSIN-INSA-Strasbourg/Hermes

commit 40ba85b4b94a8570b386c28c09de94059d033e8c

🧵 Added a protected attribute to server and clients allowing to run a desired number of iterations of mainloop. Useful only for functional tests

server/hermesserver.py
@@ -111,6 +111,9 @@ class HermesServer:
             seconds=config["hermes-server"]["updateInterval"]
         )
         """Interval between two update"""
+        self._numberOfLoopToProcess: int | None = None
+        """**For functionnal tests only**, if a value is set, will process for *value*
+        iterations of mainloop and pause execution until a new positive value is set"""
 
         self._firstFetchDone = False
         """Indicate if a full data set has been fetched since start"""
@@ -455,35 +458,43 @@ class HermesServer:
                     self.initsync()
                     self._initSyncRequested = False
 
-                if self._forceUpdate or (
-                    not self._isPaused and datetime.now() >= self._nextUpdate
+                if (
+                    not self._forceUpdate
+                    and not self._numberOfLoopToProcess
+                    and (self._isPaused or datetime.now() < self._nextUpdate)
                 ):
-                    # Standard run
-                    if self._forceUpdate:
-                        self._forceUpdate = False
-                    else:
-                        self._nextUpdate += self._updateInterval
+                    time.sleep(1)
+                    continue
 
-                    self.dm.fetch()
-                    self.generateAndSendEvents(
-                        eventCategory="base",
-                        data=self.dm.data,
-                        cache=self.dm.data.cache,
-                        save=True,
-                        commit=True,
-                        sendEvents=(self._cache.lastUpdate is not None),
-                    )
-                    self._cache.lastUpdate = datetime.now()
-                    self.notifyException(None)
-                    self._cache.savecachefile()
+                # Standard run
+                if self._forceUpdate:
+                    self._forceUpdate = False
+                else:
+                    self._nextUpdate += self._updateInterval
 
-                time.sleep(1)
+                self.dm.fetch()
+                self.generateAndSendEvents(
+                    eventCategory="base",
+                    data=self.dm.data,
+                    cache=self.dm.data.cache,
+                    save=True,
+                    commit=True,
+                    sendEvents=(self._cache.lastUpdate is not None),
+                )
+                self._cache.lastUpdate = datetime.now()
+                self.notifyException(None)
+                self._cache.savecachefile()
 
             except Exception as e:
                 lines = traceback.format_exception(type(e), e, e.__traceback__)
                 trace = "".join(lines).strip()
                 self.notifyException(trace)
                 self._cache.savecachefile()
+
+            # Only used in functionnal tests
+            if self._numberOfLoopToProcess:
+                self._numberOfLoopToProcess -= 1
+
         self._cache.savecachefile()
 
     def status(

clients/__init__.py
@@ -170,6 +170,10 @@ class GenericClient:
         self.__isStopped: bool = False
         """mainloop() will run until this var is set to True"""
 
+        self.__numberOfLoopToProcess: int | None = None
+        """**For functionnal tests only**, if a value is set, will process for *value*
+        iterations of mainloop and pause execution until a new positive value is set"""
+
         self.__sock: SockServer | None = None
         """Facultative socket to allow cli communication"""
         if self.__config["hermes"]["cli_socket"]["path"] is not None:
@@ -461,7 +465,7 @@ class GenericClient:
 
             with self._msgbus:
                 try:
-                    if self.__isPaused:
+                    if self.__isPaused or self.__numberOfLoopToProcess == 0:
                         sleep(1)
                         continue
 
@@ -514,6 +518,10 @@ class GenericClient:
                             self.__datamodel.saveLocalAndRemoteData()
                         self.__callHandler("", "save")  # Call special event "on_save()"
                         self.__notifyQueueErrors()
+
+            # Only used in functionnal tests
+            if self.__numberOfLoopToProcess:
+                self.__numberOfLoopToProcess -= 1
 
     def __retryErrorQueue(self):
         # Enforce retryInterval

commit 261611584d276345eda344dc48df2be35250f858

🐛 Fixed a bug in server's mainloop that was still processing periodic update during functional tests, instead of only processing when asked to. Bug was introduced in commit 40ba85b4b94a8570b386c28c09de94059d033e8c

server/hermesserver.py
@@ -458,11 +458,16 @@ class HermesServer:
                     self.initsync()
                     self._initSyncRequested = False
 
-                if (
-                    not self._forceUpdate
-                    and not self._numberOfLoopToProcess
-                    and (self._isPaused or datetime.now() < self._nextUpdate)
-                ):
+                if self._numberOfLoopToProcess is None:
+                    # Normal operations
+                    updateRequired = self._forceUpdate or (
+                        not self._isPaused and datetime.now() >= self._nextUpdate
+                    )
+                else:
+                    # Special case for functional tests
+                    updateRequired = self._numberOfLoopToProcess > 0
+
+                if not updateRequired:
                     time.sleep(1)
                     if self._nextUpdate + self._updateInterval < datetime.now():
                         # Keep updating _nextUpdate even when paused, ensuring that its