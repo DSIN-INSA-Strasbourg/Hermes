4139f4ca505784917f04ef49682d744acefa0c08
Boris Lechner <148430302+Boris-INSA@users.noreply.github.com>
2024-10-29 15:47:27 +0100

    🔖 Released v1.0.0-alpha.7

--- lib/version.py
+++ lib/version.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Hermes. If not, see <https://www.gnu.org/licenses/>.
 
-HERMES_VERSION = "1.0.0-alpha.6"
+HERMES_VERSION = "1.0.0-alpha.7"
 """Curent app version"""
 
 HERMES_VERSIONS = [
@@ -29,5 +29,6 @@ HERMES_VERSIONS = [
     "1.0.0-alpha.4",
     "1.0.0-alpha.5",
     "1.0.0-alpha.6",
+    "1.0.0-alpha.7",
 ]
 """History of app versions, from oldest to newest"""
