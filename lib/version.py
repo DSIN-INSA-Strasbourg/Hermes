087a8e65d70d30e9970a5a558239ec9ee7f01526
Boris Lechner <148430302+Boris-INSA@users.noreply.github.com>
2025-05-05 17:51:23 +0200

    🔖 Released v1.0.3

--- lib/version.py
+++ lib/version.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Hermes. If not, see <https://www.gnu.org/licenses/>.
 
-HERMES_VERSION = "1.0.2"
+HERMES_VERSION = "1.0.3"
 """Curent app version"""
 
 HERMES_VERSIONS = [
@@ -34,5 +34,6 @@ HERMES_VERSIONS = [
     "1.0.0",
     "1.0.1",
     "1.0.2",
+    "1.0.3",
 ]
 """History of app versions, from oldest to newest"""
